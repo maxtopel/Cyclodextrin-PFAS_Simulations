033cefea34c7807bb8953b38f888010857d66e93
maxtopel <54963055+maxtopel@users.noreply.github.com>
2024-01-09 17:31:40 +0100

    p-gcd

---
+++ system_cd/pfos-f-gcd/box-probes.sh
@@ -0,0 +1,40 @@
+module load cuda/11.3
+module load gcc/9.1.0
+
+export PATH=/work2/03273/tg825722/shared-folder-siva/software/enhancements/plumed-2.8.1/installed-files/bin:$PATH
+export LIBRARY_PATH=/work2/03273/tg825722/shared-folder-siva/software/enhancements/plumed-2.8.1/installed-files/lib:$LIBRARY_PATH
+export LD_LIBRARY_PATH=/work2/03273/tg825722/shared-folder-siva/software/enhancements/plumed-2.8.1/installed-files/lib:$LD_LIBRARY_PATH
+export PLUMED_KERNEL=/work2/03273/tg825722/shared-folder-siva/software/enhancements/plumed-2.8.1/installed-files/lib/libplumedKernel.so
+
+echo PATH=/work2/03273/tg825722/shared-folder-siva/software/enhancements/openmpi/openmpi-2.0.2/installed-files/bin:$PATH
+echo LIBRARY_PATH=/work2/03273/tg825722/shared-folder-siva/software/enhancements/openmpi/openmpi-2.0.2/installed-files/lib:$PATH
+export LD_LIBRARY_PATH=/work2/03273/tg825722/shared-folder-siva/software/enhancements/openmpi/openmpi-2.0.2/installed-files/lib:$LD_LIBRARY_PATH
+
+source /work2/03273/tg825722/shared-folder-siva/software/gromacs/gromacs-2021.6/installed-files/bin/GMXRC
+
+
+
+
+for fileName in gcd
+
+do
+#9
+gmx editconf -princ -f $fileName.gro -o $fileName-box.gro -box 7 7 7 -center 3.5 3.5 3.5 <<EOF
+0
+EOF
+
+done
+
+
+for fileName in pfos-anion
+
+do
+#SDS
+#9
+gmx editconf -f $fileName.gro -o $fileName-box.gro -box 7 7 7 -center 3.5 3.5 4.5 <<EOF
+0
+EOF
+
+
+
+done
---
+++ system_cd/pfos-f-gcd/restr.sh
@@ -0,0 +1,16 @@
+# Change the following two lines for setting up different analytes and probes
+
+analyte=pfos-anion
+probe=gcd
+
+
+gmx genrestr -f $probe-box.gro -o probe-posres.itp -fc 500 500 500 <<EOF 
+2
+EOF
+
+gmx genrestr -f $analyte-box.gro -o analyte-posres.itp -fc 500 500 500 <<EOF
+2
+EOF
+
+
+
